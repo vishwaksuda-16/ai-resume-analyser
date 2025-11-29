80913dba2c027590920e609a2db5e7a11371054d
vishwaksuda-16 <vishwaksuda@gmail.com>
2025-11-29 16:52:30 +0530

    Home Screen

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,31 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyser" },
+    { name: "description", content: "Smart Resume AI for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section max-w-6xl mx-auto px-4 md:px-8">
+          <div className="page-heading py-16">
+              <h1>Track your Applications & Resume Ratings</h1>
+              <h2>Review your submissions and check AI-powered feedback.</h2>
+          </div>
+
+      {resumes.length > 0 && (
+      <div className="resumes-section grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 pb-16">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume}/>
+          ))}
+      </div>
+      )}
+      </section>
+  </main>
 }
